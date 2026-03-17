2840136d394d9a35ff26d719bcfb6d3e4052b13c
TGH75972 <brogamersvlogs02@gmail.com>
2026-03-17 15:25:37 +0530

    commit 1

---
+++ src/main/java/com/craft/amenitycraft/AmenityCraft.java
@@ -0,0 +1,16 @@
+package com.craft.amenitycraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AmenityCraft implements ModInitializer {
+	public static final String MOD_ID = "amenitycraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+ 
+	}
+}
---
+++ src/main/java/com/craft/amenitycraft/AmenityCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.craft.amenitycraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AmenityCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
