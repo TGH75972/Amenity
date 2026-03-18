3bee64588a6c48d49075add3d0682972b4f066cc
TGH75972 <brogamersvlogs02@gmail.com>
2026-03-18 22:43:37 +0530

    Commit 5

--- src/main/java/com/craft/amenitycraft/item/HeadphonesItem.java
+++ src/main/java/com/craft/amenitycraft/item/HeadphonesItem.java
@@ -1,5 +1,27 @@
 package com.craft.amenitycraft.item;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+public class HeadphonesItem extends Item {
+public HeadphonesItem(Settings settings) {
+super(settings);
+}
 
-public class HeadphonesItem {
-    
+@Override
+public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+if (world.isClient && entity instanceof PlayerEntity player) {
+ItemStack discStack = player.getInventory().getStack(0);
+if (discStack.contains(DataComponentTypes.JUKEBOX_PLAYABLE)) {
+boolean isWearing = player.getEquippedStack(EquipmentSlot.HEAD) == stack;
+
+if (isWearing) {
+    player.getAbilities().allowFlying = player.getAbilities().allowFlying;
+}
+  }
+ }
+}
 }
